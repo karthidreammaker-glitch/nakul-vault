167a492cba666d51952f4908d00fd3aaad0ff0e2
karthidreammaker-glitch <karthidreammaker@gmail.com>
2026-06-24 14:47:12 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import streamlit.components.v1 as components
 
-st.set_page_config(page_title="👶 Nakul Reveal!", layout="centered", page_icon="✨")
+st.set_page_config(page_title="👶 Name Reveal!", layout="centered", page_icon="✨")
 
 st.markdown("""
     <style>
